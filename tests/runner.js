7d5864fa7a76f81ef319a407eec211a70cfee359
mrarvind90 <arvindrankalidas@gmail.com>
2023-09-26 14:48:33 +0800

    Updated tests to load and run es6 modules

--- tests/runner.js
+++ tests/runner.js
@@ -26,32 +26,37 @@ emitter.run = function () {
 	const separator = ' -> ';
 	// Run the tests.
 	mocha
-		.ui('tdd')
-		.run()
-		.on('test end', function (test) {
-			// remove comments
-			let body = test.body.replace(/\/\/.*\n|\/\*.*\*\//g, '');
-			// collapse spaces
-			body = body.replace(/\s+/g, ' ');
-			const obj = {
-				title: test.title,
-				context: context.slice(0, -separator.length),
-				state: test.state,
-				// body: body,
-				assertions: assertionAnalyser(body),
-			};
-			tests.push(obj);
-		})
-		.on('end', function () {
-			emitter.report = tests;
-			emitter.emit('done', tests);
-		})
-		.on('suite', function (s) {
-			context += s.title + separator;
-		})
-		.on('suite end', function (s) {
-			context = context.slice(0, -(s.title.length + separator.length));
-		});
+		.loadFilesAsync()
+		.then(() =>
+			mocha
+				.ui('tdd')
+				.run()
+				.on('test end', function (test) {
+					// remove comments
+					let body = test.body.replace(/\/\/.*\n|\/\*.*\*\//g, '');
+					// collapse spaces
+					body = body.replace(/\s+/g, ' ');
+					const obj = {
+						title: test.title,
+						context: context.slice(0, -separator.length),
+						state: test.state,
+						// body: body,
+						assertions: assertionAnalyser(body),
+					};
+					tests.push(obj);
+				})
+				.on('end', function () {
+					emitter.report = tests;
+					emitter.emit('done', tests);
+				})
+				.on('suite', function (s) {
+					context += s.title + separator;
+				})
+				.on('suite end', function (s) {
+					context = context.slice(0, -(s.title.length + separator.length));
+				}),
+		)
+		.catch(() => (process.exitCode = 1));
 };
 
 export default emitter;
